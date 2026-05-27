363ac5e04a9ba1e267b4b0453a8f6c6afc831055
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-05-27 16:33:42 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -2,14 +2,21 @@
 #include <string.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 3)
+    if(argc < 3)
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
+    char buffer[256];
 
+    while(feof(fp) == 0){
+        fgets(buffer, sizeof(buffer), fp);
+        if(strchr(buffer, *target_str) == 0){
+            printf("%s\n", buffer);
+        }
+    }
 
-    
     fclose(fp);
-}
 
+    return 0;
+}
