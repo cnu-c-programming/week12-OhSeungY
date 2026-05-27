10a15ad8c4e38f90f41aea09d8951d99afcf6ff6
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-05-27 19:08:04 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -10,7 +10,33 @@ typedef struct config {
 } Config;
 
 void config_parser(Config* config_ptr) {
-
+    FILE* fp = fopen("config.txt", "r");
+    char buffer[256];
+    char value[64];
+    int count = 1;
+   
+    while(feof(fp) == 0){
+        fgets(buffer, sizeof(buffer), fp);
+        if(count == 1){
+            sscanf(buffer, "InputFileName=%s", value);
+            strcpy(config_ptr->InputFileName, value);
+            count++;
+        }else if(count == 2){
+            sscanf(buffer, "Options=%s", value);
+            config_ptr->Options = atoi(value);
+            count++;
+        }else if(count == 3){
+            sscanf(buffer, "SectionName=%s", value);
+            strcpy(config_ptr->SectionName, value);
+            count++;
+        }else if(count == 4){
+            sscanf(buffer, "Address=%s", value);
+            config_ptr->Address = strtol(value, NULL, 16);
+            
+        }
+    }
+    fclose(fp);
+    
 }
 
 int main(int argc, const char* argv[]) {
