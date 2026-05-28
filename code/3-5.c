51677388cdfe375f6c019e0e18c804943e093e8e
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-05-28 12:40:52 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
 
     FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
+    if(fp == NULL){
         return 0;
     }
 
-
-    int sum = 0;
     char buffer[32];
+    int sum = 0;
     while(feof(fp) == 0){
         fgets(buffer, sizeof(buffer), fp);
         if(isdigit(*buffer)){
             sum += atoi(buffer);
         }else{
-            printf("invalid input %s", buffer);
+            fprintf(stderr, "invalid input %s", buffer);
         }
     }
+    printf("sum: %d", sum);
 
-
-    printf("sum: %d\n", sum);
     fclose(fp);
+
     return 0;
 }
