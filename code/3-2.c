af3858c9ef16a950808ae0bfd4e22a92239a6131
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-05-28 12:17:06 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    
-    if (argc < 2)
+    if(argc < 2)
         return 0;
-    
+
     const char* filename = argv[1];
     char buffer[10] = {0};
 
     FILE* fp = fopen(filename, "r");
-    if(fp = NULL){
+    if(fp == NULL){
         return 0;
     }
 
@@ -20,4 +19,3 @@ int main(int argc, const char* argv[]) {
 
     return 0;
 }
-
