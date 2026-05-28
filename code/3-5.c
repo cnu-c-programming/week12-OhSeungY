2121dd894dfd0432644dd01aebedd476ead06d03
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-05-28 16:45:01 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
@@ -15,7 +16,14 @@ int main(int argc, const char* argv[]) {
 
     while(feof(fp) == 0){
         fgets(buffer, sizeof(buffer), fp);
-        if(isdigit(*buffer)){
+        int isDigit = 1;
+        int str_length = strlen(buffer) - 1;
+        for(int i=0; i<str_length; i++){
+            if(isdigit(*(buffer + i)) == 0){
+                isDigit = 0;
+            }
+        }
+        if(isDigit == 1){
             sum += atoi(buffer);
         }else{
             fprintf(stderr, "invalid input %s", buffer);
