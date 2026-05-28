4a9f4b7ddf629b6f0d5a4ae165768408be773de5
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-05-28 12:18:49 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -5,8 +5,8 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
-    if(fp = NULL){
-        perror("file does not exists");
+    if(fp == NULL){
+        perror("file dose not exists");
         return 1;
     }
 
